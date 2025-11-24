3a473b50ef0468ef631a80843c0794bfd21b0021
Dulguun <dulguunmunkhbold0@gmail.com>
2025-11-24 21:24:14 +0800

    active player solidog.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэе
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -22,11 +22,32 @@ document.getElementById("current-1").textContent = '0';
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
-//Эвэнт гэдэг нь ямар нэгэн үйлдэл хийгдлээ гэж кодонд мэдэгддэг процесс юм. Жишээ нь click, scroll, press mouse
-// roll dice button дарахад event ажиллах
+// roll dice button дарахад event ажиллах ба шоо шидэх эвэнт байна буюу голын шооны зураг хөдөлнө.
 document.querySelector(".btn-roll").addEventListener("click", function () {
+    //1 - 6 доторх санамсаргүй тоо generates
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     diceDom.style.display = "block";
     diceDom.src = "dice-" + diceNumber + ".png";
 
+    // буусан тоо нь нэгээс ялгаатай тоглогчийн ээлжийн оноог нэмэгдүүлнэ
+    if (diceNumber !== 1) {
+        roundScore += diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    }
+    else {
+
+        scores[activePlayer] += roundScore;
+        roundScore = 0;
+        document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+        // нэг буусан бол тоглогчийн ээлжийг солино
+        //Хэрэв идэвхтэй тоглогч нь О байвал 1 болго. Үгүй бол О болго.
+
+        //before changing activePlayer. Remove red dote.
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        // Red dot 
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("active");
+
+    }
 });
