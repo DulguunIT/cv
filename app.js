41756ab592209ea3f9c39abc2c985db230620d77
Dulguun <dulguunmunkhbold0@gmail.com>
2025-11-24 20:22:18 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,18 +8,25 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, нэгээс зургаа гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
-console.log("Шоо: " + dice);
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 /* <div class="player-score" id="score-0">43</div> */
 // window.document.querySelector('#score-0').textContent = dice;
 // window.document.querySelector('#score-1').innerHTML = "<em> YES </em>";
 
 //Программ эхлэхэд бэлтгье
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
+document.getElementById("score-0").textContent = '0';
+window.document.getElementById('score-1').textContent = '0';
 // <div class="player-current-score" id="current-0">11</div> 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
 //Эвэнт гэдэг нь ямар нэгэн үйлдэл хийгдлээ гэж кодонд мэдэгддэг процесс юм. Жишээ нь click, scroll, press mouse
+// roll dice button дарахад event ажиллах
+document.querySelector(".btn-roll").addEventListener("click", function () {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+});
