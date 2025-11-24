408316790383abae9cce62074176b8e0d3208a9d
Dulguun <dulguunmunkhbold0@gmail.com>
2025-11-24 19:50:12 +0800

    using git for my folder

---
+++ app.js
@@ -0,0 +1,25 @@
+//Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэе
+var activePlayer = 1;
+
+//Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+
+//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+
+//Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, нэгээс зургаа гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var dice = Math.floor(Math.random() * 6) + 1;
+console.log("Шоо: " + dice);
+/* <div class="player-score" id="score-0">43</div> */
+// window.document.querySelector('#score-0').textContent = dice;
+// window.document.querySelector('#score-1').innerHTML = "<em> YES </em>";
+
+//Программ эхлэхэд бэлтгье
+window.document.querySelector('#score-0').textContent = 0;
+window.document.querySelector('#score-1').textContent = 0;
+// <div class="player-current-score" id="current-0">11</div> 
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+document.querySelector(".dice").style.display = "none";
+
+//Эвэнт гэдэг нь ямар нэгэн үйлдэл хийгдлээ гэж кодонд мэдэгддэг процесс юм. Жишээ нь click, scroll, press mouse
