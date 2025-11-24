57796a4137186ea0b8e1f462382c37f6dfa780be
Dulguun <dulguunmunkhbold0@gmail.com>
2025-11-24 22:20:27 +0800

    Game is almost done! Error is even if the someone win, the game is continueing!

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ document.getElementById("current-1").textContent = '0';
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+
+
 // roll dice button дарахад event ажиллах ба шоо шидэх эвэнт байна буюу голын шооны зураг хөдөлнө.
 document.querySelector(".btn-roll").addEventListener("click", function () {
     //1 - 6 доторх санамсаргүй тоо generates
@@ -35,19 +37,43 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
         document.getElementById("current-" + activePlayer).textContent = roundScore;
     }
     else {
-
-        scores[activePlayer] += roundScore;
         roundScore = 0;
-        document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
         document.getElementById("current-" + activePlayer).textContent = roundScore;
         // нэг буусан бол тоглогчийн ээлжийг солино
         //Хэрэв идэвхтэй тоглогч нь О байвал 1 болго. Үгүй бол О болго.
-
-        //before changing activePlayer. Remove red dote.
-        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        // Red dot 
-        document.querySelector(".player-" + activePlayer + "-panel").classList.add("active");
-
+        switchToNextPlayer();
     }
+
 });
+
+//hold button event
+document.querySelector('.btn-hold').addEventListener("click", function () {
+    // global score is added
+    scores[activePlayer] += roundScore;
+    if (checkWin() === true) return;
+    //eeljiin onoo O bolood , hevlene.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    switchToNextPlayer();
+}
+);
+//Тоглогчийг өөрлөх
+function switchToNextPlayer() {
+    //before changing activePlayer. Remove red dote.
+    document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // Red dot is add for new active player
+    document.querySelector(".player-" + activePlayer + "-panel").classList.add("active");
+    diceDom.style.display = "none";
+}
+function checkWin() {
+    //Checking win 
+    if (scores[activePlayer] >= 20) {
+        document.getElementById("name-" + activePlayer).textContent = "WINNER";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        diceDom.style.display = "none";
+        return true;
+    }
+}
